72b4668885f3393769d1244cf963bd2479259a28
galannp <82237388+galannp@users.noreply.github.com>
2021-04-26 19:01:26 +0200

    Update User.java

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,8 +1,6 @@
 package entities;
 
 public abstract class User {
-	private String username;
-	private String password;
 	private String first_name;
 	private String last_name;
 	private String email;
@@ -10,23 +8,13 @@ public abstract class User {
 	private String status;
 	//LIST OF FRIENDS ......
 	
-	protected User(String user, String pass, String first, String last, String mail, String phone) {
-		this.username = user; 
-		this.password = pass;
+	protected User(String first, String last, String mail, String phone) {
 		this.first_name = first;
 		this.last_name = last;
 		this.email = mail;
 		this.phone_number = phone;
 	}
-	
-	public String getUsername()
-	{
-		return this.username;
-	}
-	public String getPassword()
-	{
-		return this.password;
-	}
+
 	public String getFirstName()
 	{
 		return this.first_name;
@@ -42,12 +30,6 @@ public abstract class User {
 	public String getPhoneNumber()
 	{
 		return this.phone_number;
-	}
-	public void setUsername(String change) {
-		this.username = change;
-	}
-	public void setPassword(String change) {
-		this.password = change;
 	}
 	public void setFirstName(String change) {
 		this.first_name = change;
