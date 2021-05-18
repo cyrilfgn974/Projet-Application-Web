49a34833bfc251cdab6e48c310e88283adaff8a3
galannp <82237388+galannp@users.noreply.github.com>
2021-05-18 23:50:31 +0200

    Update Administrator.java

--- src/entities/Administrator.java
+++ src/entities/Administrator.java
@@ -1,5 +1,9 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Administrator extends User {
 
 	protected Administrator(String user, String pass, String first, String last, String mail, String phone) {
