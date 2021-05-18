ec8dff1c5d5793ab99159fff6a1b74612ec3be23
galannp <82237388+galannp@users.noreply.github.com>
2021-05-18 23:44:30 +0200

    Update Artist.java

--- src/entities/Artist.java
+++ src/entities/Artist.java
@@ -4,10 +4,8 @@ import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Artist extends User {
