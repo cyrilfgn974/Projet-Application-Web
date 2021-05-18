12b9085ef6c1c334e5565fc6aeb2245fb92e03e3
galannp <82237388+galannp@users.noreply.github.com>
2021-05-18 23:47:46 +0200

    Update Muse.java

--- src/entities/Muse.java
+++ src/entities/Muse.java
@@ -1,9 +1,28 @@
 package entities;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Muse extends User {
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	Collection<Artist> artistes;
 
 	protected Muse(String user, String pass, String first, String last, String mail, String phone) {
 		super(user, pass, first, last, mail, phone);
 	}
-	
+
+	public Collection<Artist> getArtistes() {
+		return artistes;
+	}
+
+	public void setArtistes(Collection<Artist> artistes) {
+		this.artistes = artistes;
+	}
+
 }
