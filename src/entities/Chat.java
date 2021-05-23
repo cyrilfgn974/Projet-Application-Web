49bdc55d32a64c0f4864a3635ce368ea2279f664
galannp <82237388+galannp@users.noreply.github.com>
2021-05-23 23:58:33 +0200

    Update Chat.java

--- src/entities/Chat.java
+++ src/entities/Chat.java
@@ -1,7 +1,13 @@
 package entities;
 
+import java.util.List;
+
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Chat {
@@ -14,9 +20,11 @@ public class Chat {
     private int id;
     private User user1;
     private User user2;
-    private messages;
 
-    public Money(long unnormValue, String currency) {
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Message> messages;
+
+    public Chat(User user1, User user2) {
         this.user1 = user1;
         this.user2 = user2;
     }
@@ -29,19 +37,23 @@ public class Chat {
         return user2;
     }
 
-    public getMessages() {
+    public List<Message> getMessages() {
         return messages;
     }
 
-    public void setUser1() {
+    public void setUser1(User user1) {
         this.user1 = user1;
     }
 
-    public void setUser2() {
+    public void setUser2(User user2) {
         this.user2 = user2;
     }
 
-    public void setMessages() {
-        this.messages = messages;
+    public void setMessages(List<Message> messages) {
+        try {
+            this.messages = messages.subList(0, NB_CONSERVES);
+        } catch (IndexOutOfBoundsException e) {
+            this.messages = messages;
+        }
     }
 }
